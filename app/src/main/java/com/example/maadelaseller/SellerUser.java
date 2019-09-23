e0cd7c28dee18db06503a2341fb916a4c0b3d850
Thanislaus Cleman <cthanislaus@gmail.com>
2019-09-23 14:20:36 +0530

    Ad phone verify check 123

--- app/src/main/java/com/example/maadelaseller/SellerUser.java
+++ app/src/main/java/com/example/maadelaseller/SellerUser.java
@@ -4,6 +4,24 @@ public class SellerUser {
     private String id;
     private String shopname;
     private String phonenum;
+    private String Address;
+    private String AccNo;
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getAccNo() {
+        return AccNo;
+    }
+
+    public void setAccNo(String accNo) {
+        AccNo = accNo;
+    }
 
     public String getId() {
         return id;
